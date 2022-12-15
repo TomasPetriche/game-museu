f25d9cab342499db80a8aaaea592049f75cca40b
ToxicMask <tomas.carciofi@gmail.com>
2022-12-15 13:34:10 -0300

    ArtefactPick Up 2.0

--- scenes/game/artefact/artefact_pick_up.gd
+++ scenes/game/artefact/artefact_pick_up.gd
@@ -1,2 +1,14 @@
 extends Area2D
 class_name ArtefactPickUp
+
+signal picked_up
+
+func _ready():
+	var _err = connect("body_entered", self, "_check_pick_up")
+
+func _check_pick_up(body:Node):
+
+	if body is KinematicBody2D:
+		emit_signal("picked_up")
+		queue_free()
+	pass 
