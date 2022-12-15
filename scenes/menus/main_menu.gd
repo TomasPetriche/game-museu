5f38e6010b529721402a2d7793135943ee0b79fa
ToxicMask <42303435+ToxicMask@users.noreply.github.com>
2022-12-15 16:56:15 -0300

    Main + New UI inputs

--- scenes/menus/main_menu.gd
+++ scenes/menus/main_menu.gd
@@ -1,10 +1,20 @@
 extends Control
+class_name MainMenu
+
+export (NodePath) var quit_path : NodePath = ""
+
+func _ready():
+	if OS.has_feature("web"):
+		var quit_button = get_node_or_null(quit_path)
+		if quit_button:
+			quit_button.queue_free()
+	pass
 
 
 func _start_game():
-	print("START")
+	get_tree().call_group("Main", "go_to_scene", "test0")
 	pass
 
 func _quit():
-	get_tree().quit()
+	get_tree().call_group("Main", "quit_app")
 	pass
