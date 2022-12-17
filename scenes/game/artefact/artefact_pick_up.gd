d3c620c45c0000de63c2b33a1c71336bb23cccaf
ToxicMask <tomas.carciofi@gmail.com>
2022-12-17 12:55:43 -0300

    Artefact Data + Popup Improv.

--- scenes/game/artefact/artefact_pick_up.gd
+++ scenes/game/artefact/artefact_pick_up.gd
@@ -3,18 +3,22 @@ class_name ArtefactPickUp
 
 signal picked_up
 
-export (String) var name_info : String = "NAME_TEST"
-export (String) var text_info : String = "TEXT_DESCRIPTION"
-export (Texture) var icon_texture : Texture = null
+export (Resource) var artefact_data 
 
 func _ready():
 	var _err = connect("body_entered", self, "_check_pick_up")
+	if artefact_data:
+		artefact_data = artefact_data as ArtefactData
+		$Sprite.texture = artefact_data.sprite_texture
+	pass
 
 func _check_pick_up(body:Node):
 	if body is KinematicBody2D:
 		emit_signal("picked_up")
+		if artefact_data:
 		# Call an call_group with a null argument, does not work
-		get_tree().call_group("ArtefactPopup", "_popup_pick_up", name_info, text_info, icon_texture)
+			artefact_data = artefact_data as ArtefactData
+			get_tree().call_group("ArtefactPopup", "_popup_pick_up", artefact_data.name_info, artefact_data.text_info, artefact_data.icon_texture)
 		yield(get_tree(),"idle_frame")
 		queue_free()
 	pass 
