710f760a52a7e25c0a2a3bd341b0290ee199ceac
ToxicMask <42303435+ToxicMask@users.noreply.github.com>
2022-12-17 12:13:21 -0300

    Artefact Popup

--- scenes/game/artefact/artefact_pick_up.gd
+++ scenes/game/artefact/artefact_pick_up.gd
@@ -3,11 +3,18 @@ class_name ArtefactPickUp
 
 signal picked_up
 
+export (String) var name_info : String = "NAME_TEST"
+export (String) var text_info : String = "TEXT_DESCRIPTION"
+export (Texture) var icon_texture : Texture = null
+
 func _ready():
 	var _err = connect("body_entered", self, "_check_pick_up")
 
 func _check_pick_up(body:Node):
 	if body is KinematicBody2D:
 		emit_signal("picked_up")
+		# Call an call_group with a null argument, does not work
+		get_tree().call_group("ArtefactPopup", "_popup_pick_up", name_info, text_info, icon_texture)
+		yield(get_tree(),"idle_frame")
 		queue_free()
 	pass 
---
+++ scenes/game/hud/artefact_popup.gd
@@ -0,0 +1,18 @@
+extends Popup
+class_name ArtefactPopup
+
+export (NodePath) var nameLabel : NodePath
+export (NodePath) var textLabel : NodePath
+export (NodePath) var iconTexture : NodePath
+
+
+func _popup_pick_up(name: String, fill_text: String, icon_texture : Texture):
+	print("@")
+	if nameLabel:
+		(get_node(nameLabel) as Label).text = "Você encontrou %s!" % [name]
+	if textLabel:
+		(get_node(textLabel) as Label).text = fill_text
+	if iconTexture:
+		(get_node(iconTexture) as TextureRect).texture = icon_texture
+		self.popup()
+	pass
