5adab9d5186c690f0e1b05b411f492445da546d3
ToxicMask <tomas.carciofi@gmail.com>
2023-04-26 00:16:21 -0300

    Possible animation fix

--- scenes/game/player/player_avatar.gd
+++ scenes/game/player/player_avatar.gd
@@ -10,7 +10,9 @@ func _set_active(value):
 func _fade():
 	if active:
 		self.active = false
+		$PlayerMovement/MoveTween.stop_all()
 		$PlayerController.queue_free()
 		$PlayerMovement.queue_free()
+		yield(get_tree(), "idle_frame")
 		$PlayerAnimation.play("player_fade")
 	pass
